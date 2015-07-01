14defc2e43ace135a54f376129607eff320e81e3
otamm <oivatom@gmail.com>
2015-07-01 00:42:13 -0400

    Created 'Character' and 'Piece' files to contain classes

---
+++ Character.swift
@@ -0,0 +1,9 @@
+//
+//  Character.swift
+//  SushiNeko
+//
+//  Created by Otavio Monteagudo on 7/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
---
+++ Piece.swift
@@ -0,0 +1,9 @@
+//
+//  Piece.swift
+//  SushiNeko
+//
+//  Created by Otavio Monteagudo on 7/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
