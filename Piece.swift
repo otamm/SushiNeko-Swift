9a1b6226fa60c2d890cf5b985ff7f48abf1de0bb
otamm <oivatom@gmail.com>
2015-07-01 06:48:03 -0400

    Implemented randomized obstacles

--- Piece.swift
+++ Piece.swift
@@ -10,4 +10,49 @@ import Foundation;
 
 class Piece:CCNode {
     
+    /* code connections */
+    // left chopstick of a piece
+    weak var leftChopstick: CCSprite!;
+    
+    // right chopstick of a piece, never to be alongside a left one (or vice-versa)
+    weak var rightChopstick: CCSprite!;
+    
+    /* custom variables */
+    
+    // side of the chopstick in the current piece; there should never be two chopsticks at once.
+    var side: Side = .None {
+        // didSet block is called each time 'side' is set; will set the other chopstick visibility to 'false' (two chopsticks are created for each Piece instance, only the visible triggers a 'game over'
+        didSet {
+            self.leftChopstick.visible = false;
+            self.rightChopstick.visible = false;
+            if self.side == .Right {
+                self.rightChopstick.visible = true;
+            } else if self.side == .Left {
+                self.leftChopstick.visible = true;
+            }
+        }
+    }
+    
+    /*** methods ***/
+    
+    /* custom methods */
+    
+    // will set obstacle side for this Piece instance.
+    func setObstacle(lastSide: Side) -> Side {
+        if (lastSide != .None) {
+            self.side = .None;
+        } else {
+            var rand = CCRANDOM_0_1()
+            if rand < 0.45 {
+                self.side = .Left;
+            } else if rand < 0.9 {
+                self.side = .Right;
+            } else {
+                self.side = .None;
+            }
+        }
+        return self.side;
+    }
+    
+    
 }
--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -1,10 +1,19 @@
 import Foundation;
 
+
+
+enum Side {
+    case Left, Right, None
+}
+
 class MainScene: CCNode {
     /* custom variables */
     
     // an array to hold Piece class instances
     var pieces:[Piece] = [];
+    
+    // last piece to be generated. Set to 'left' so first piece will always be a .None
+    var pieceLastSide: Side = .Left;
     
     /* code connections; variables must have 'weak' before being declared because they are only pointing to a reference of the object, not storing the actual data. */
     
@@ -13,6 +22,8 @@ class MainScene: CCNode {
     
     // the character instance for the game session
     weak var character:Character!;
+    
+    
     /** methods **/
     
     
@@ -25,12 +36,15 @@ class MainScene: CCNode {
         // adds 10 pieces to scene
         for i in 0..<10 {
             var piece = CCBReader.load("Piece") as! Piece;
+            self.pieceLastSide = piece.setObstacle(pieceLastSide); // both randomizes current chopstick side and updates value of pieceLastSide
             // position will be relative to 'piecesNode' container.
             piece.position = CGPoint(x:0 ,y:CGFloat(pieceYPosition));
             pieceYPosition += piece.contentSizeInPoints.height;
             self.piecesNode.addChild(piece);
             self.pieces.append(piece);
         }
+        
+        
         
         self.userInteractionEnabled = true;
         
